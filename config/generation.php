1a40307c17d3e49aedc080fa498aaa623b48a75b
KimSpeer <kim.speer@co-IT.eu>
2023-03-03 16:20:55 +0100

    init

--- config/generation.php
+++ config/generation.php
@@ -2,7 +2,7 @@
 
 return [
     [
-        'source' => __DIR__.'/../node_modules/webicons',
+        'source' => __DIR__.'/../node_modules/adrolli-flag-icons/flags/1x1',
         'destination' => __DIR__.'/../resources/svg',
     ],
 ];
